f33c8d76c8b8ba3452972419b83217e6bd5019e8
Sam Dammers <sam.a.dammers@gmail.com>
2026-09-05 21:46:21 +1000

    Remove unused name_prefix local (tflint: terraform_unused_declarations)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,6 +1,5 @@
 locals {
-  name_prefix = "valheim"
-  region      = var.aws_region
+  region = var.aws_region
 
   server_fqdn = "valheim.${var.domain}"
   api_fqdn    = "api.valheim.${var.domain}"
